46c996a34f6ad1a3d8c3c985d12351c2306a4fc9
Felix <felix@felixleitner.com>
2021-09-29 20:11:02 -0400

    testing

--- builds.py
+++ builds.py
@@ -5,7 +5,7 @@ import random
 import json
 
 # Functions
-def get_prefix(bot, ctx):
+def get_prefix(bot=None, ctx=None):
     with open('data/prefixes.json', 'r') as f:
         prefixes = json.load(f)
 
