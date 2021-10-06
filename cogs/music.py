5f1db8d275816626aa33b1f292b7b92d7c51da70
Felix <felix@felixleitner.com>
2021-10-05 22:15:48 -0400

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -229,6 +229,7 @@ class Music(commands.Cog):
             return await ctx.send('You must be in the same channel as the bot to use this command!')
         
         time = parse_time(pos)
+        await ctx.send(time)
         if not time:
             return await ctx.send("Try again using either `seek (position in seconds)` or `seek (min):(sec)`")
         try:
