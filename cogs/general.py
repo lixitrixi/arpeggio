eacba04b890acdac28cc0642e73068e9270d2250
Felix <felix@felixleitner.com>
2021-10-28 18:21:49 -0400

    added invite and vote commands

--- cogs/general.py
+++ cogs/general.py
@@ -3,6 +3,16 @@ import discord
 from discord.ext import commands
 import json
 
+infoEmbed = discord.Embed(
+    colour=discord.Colour.from_rgb(255, 60, 60)
+)
+
+# infoEmbed.set_thumbnail(url='https://cdn.discordapp.com/attachments/566302530053734450/740368259584622592/Anime_Girl_Arppegio.png')
+infoEmbed.add_field(name="About", value=f"Arpeggio is a simple-to-use music bot that comes with everything you need to streamline your music playing experience!", inline=False)
+infoEmbed.add_field(name="Credits", value="Developed with invaluable help from the amazing volunteers at the Pythonista Guild.", inline=False)
+infoEmbed.add_field(name="Links", value="[Support Server](https://discord.gg/fmPTwfw) | [Invite!](https://discord.com/api/oauth2/authorize?client_id=732712093756948579&permissions=3427392&scope=bot) | [Vote!](https://top.gg/bot/732712093756948579/vote) | [GitHub](https://github.com/lixitrixi/arpeggio)")
+
+
 # Cog
 class General(commands.Cog):
     
@@ -30,16 +40,16 @@ class General(commands.Cog):
     
     @commands.command(aliases=['invite'])
     async def info(self, ctx):
-        infoEmbed = discord.Embed(
-            colour=discord.Colour.from_rgb(255, 60, 60)
-        )
-
-        # infoEmbed.set_thumbnail(url='https://cdn.discordapp.com/attachments/566302530053734450/740368259584622592/Anime_Girl_Arppegio.png')
-        infoEmbed.add_field(name="About", value=f"Arpeggio is a simple-to-use music bot that comes with everything you need to streamline your music playing experience!", inline=False)
-        # infoEmbed.add_field(name="Credits", value="Developed by <@!402954494406819862> with invaluable help from the amazing volunteers at the Pythonista Guild.\n\n(Visuals by <@!454041644912738324>)", inline=False)
-        infoEmbed.add_field(name="Links", value="[Support Server](https://discord.gg/fmPTwfw) | [Invite!](https://discord.com/api/oauth2/authorize?client_id=732712093756948579&permissions=3427392&scope=bot) | [Vote!](https://top.gg/bot/732712093756948579/vote) | [GitHub](https://github.com/lixitrixi/arpeggio)")
-
         await ctx.send(embed=infoEmbed)
+    
+    @commands.command()
+    async def invite(self, ctx):
+        await ctx.send("Invite me here! https://discord.com/api/oauth2/authorize?client_id=732712093756948579&permissions=3427392&scope=bot")
+    
+    @commands.command()
+    async def vote(self, ctx):
+        await ctx.send("Vote for me here! https://top.gg/bot/732712093756948579/vote")
+
 
     # Errors
     @changePrefix.error
