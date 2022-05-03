a23b0eeb70e6fef4630e111f2c9905f9cafac8ff
Felix <felix@felixleitner.com>
2022-05-02 20:44:05 -0400

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -73,6 +73,12 @@ class Music(commands.Cog):
                 raise Exception("StealingBot")
             await vc.disconnect()
             vc: Player = await channel.connect(cls=Player())
+
+        client = ctx.guild.voice_client
+        if client:
+            channel = client.channel
+            await client.main_ws.voice_state(ctx.guild.id, channel.id, self_deafen=True)
+
         return vc
     
     @commands.command(aliases=['leave', 'l'])
